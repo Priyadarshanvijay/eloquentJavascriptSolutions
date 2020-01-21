4a81f4daa2aa5e87a472c39163eed596e9e3ca03
Priyadarshan Vijay <priyadarshan.v@talview.com>
2020-01-21 18:49:28 +0530

    Minor Changes to Chapter 5, Dominant Writing Direction Question

--- Ch05/dominantWritingDirection.js
+++ Ch05/dominantWritingDirection.js
@@ -1,15 +1,30 @@
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let direction = groupName(item);
+    let known = counts.findIndex(c => c.direction == direction);
+    if (known == -1) {
+      counts.push({direction, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+
 function textScripts(text) {
   let scripts = countBy(text, char => {
     let script = characterScript(char.codePointAt(0));
     return script ? script.direction : "none";
-  }).filter(({name}) => name != "none");
+  });
 
   let total = scripts.reduce((n, {count}) => n + count, 0);
   if (total == 0) return "No scripts found";
 
-  return scripts.map(({name, count}) => {
+  return scripts.map(({direction, count}) => {
     return { "cnt" : Math.round(count * 100 / total),
-              "scr" : name};
+              "scr" : direction};
   });
 }
 
